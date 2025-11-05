ba9645b38de4333b22bcc0d4e6ce33124fef92c7
Ravichandra-06 <ravichandra06062006@gmail.com>
2025-11-05 07:11:42 +0530

    Add program to find max and sum of integers
    This program calculates the sum and maximum of user-input integers.

---
+++ To find max and sum.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *arr;
+    int n, i;
+    int sum = 0, max;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", (arr + i));
+    }
+
+    max = *arr;
+    for (i = 0; i < n; i++) {
+        sum += *(arr + i);
+        if (*(arr + i) > max)
+            max = *(arr + i);
+    }
+
+    printf("Sum = %d\n", sum);
+    printf("Maximum = %d\n", max);
+
+    free(arr);
+    return 0;
+}
