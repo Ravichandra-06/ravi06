1f05938c00ea71c1ab9e313450221af0c83edc11
Ravichandra-06 <ravichandra06062006@gmail.com>
2025-11-05 14:50:21 +0530

    Implement node creation using a structure in C

---
+++ Create node using structure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head->data = 10;
+    head->next = NULL;
+    printf("Data = %d\n", head->data);
+    free(head);
+
+    return 0;
+}
