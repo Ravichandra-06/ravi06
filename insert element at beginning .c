210eab0adafb869273309814a1b662842f3bf2c9
Ravichandra-06 <ravichandra06062006@gmail.com>
2025-11-05 07:19:04 +0530

    Add element insertion at the beginning of an array
    Implemented functionality to insert an element at the beginning of an array.

---
+++ insert element at beginning .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define CAP 10
+int main() {
+    int a[CAP]={20,30,40};
+    int n=3;
+    int x=90;
+    if(n<CAP){
+        for( int i=n;i>0;i--){
+            a[i]=a[i-1];
+        }
+            a[0]=x;
+            n++;
+    }
+    else{
+        printf("Array at ");
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
